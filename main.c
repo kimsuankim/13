11eae576e68753bd065eaee2f41eabeba4e48e73
kimsuankim <144988337+kimsuankim@users.noreply.github.com>
2023-11-30 15:28:50 +0900

    02

--- main.c
+++ main.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct student {
-	int id; //학번 
-	char name[100]; //이름 
-	float score; //학점 
-	
+struct point{
+	int x;
+	int y;
 };
 
 int main(int argc, char *argv[]) {
 	
-	//instance 선언, 초기화 
-	struct student s1 = {2310131, "suan", 4.3 };
+	//instance 선언 
+	struct point p1, p2;
+	int xdiff, ydiff;  
+	double dist;
 	
-	//다른값 변경
-	s1.id = 2210131;
-	s1.name[0] = 'c';
-	s1.score = 4.5;
+	//좌표 읽기 
+	printf("input p1 coordinate (x y) :");
+	scanf("%d %d", &p1.x, &p1.y);
 	
-	//출력 
-	printf("student id:&i, name:%s, score:&f", s1.id, s1.name, s1.score);
+	printf("input p2 coordinate (x y) :");
+	scanf("%d %d", &p2.x, &p2.y);
 	
-	//이름통으로변경 
-	//strcpy(s1.name, "swan");printf("name2 : %s\n");
+	//distance 계산
+	xdiff = p2.x - p1.x;
+	ydiff = p2.y - p1.y;
+	dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+	
+	printf("distance : %f\n", dist);
 	
 	system("pause");
 	return 0;
